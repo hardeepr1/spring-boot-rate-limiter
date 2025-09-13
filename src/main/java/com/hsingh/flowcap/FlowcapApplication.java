3d771e555c192c586198a13effd792fe03cefbac
Hardeep Raike <hardeeprai234@gmail.com>
2025-09-13 09:59:22 -0400

    Initialize project

---
+++ src/main/java/com/hsingh/flowcap/FlowcapApplication.java
@@ -0,0 +1,13 @@
+package com.hsingh.flowcap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowcapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowcapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsingh/flowcap/FlowcapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsingh.flowcap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowcapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
